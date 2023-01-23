31edbf106744ac86e665a3fa27a6eb9f9cb2ebd7
salamoposoDev <salamoposo.dev@gmail.com>
2023-01-23 16:33:17 +0800

    new update ui sensor tanaman

--- lib/pages/plant_sensor_page.dart
+++ lib/pages/plant_sensor_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:seirs_app/util/sensor_plant.dart';
 
 class PlantSensors extends StatefulWidget {
@@ -11,6 +12,49 @@ class PlantSensors extends StatefulWidget {
 class _PlantSensorsState extends State<PlantSensors> {
   @override
   Widget build(BuildContext context) {
-    return PlantSensorComp();
+    return SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 10.0),
+            child: Text(
+              'Plnts Sensors',
+              style: GoogleFonts.bebasNeue(
+                fontSize: 30,
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Container(
+              height: 200,
+              decoration: BoxDecoration(
+                color: Colors.blueGrey[200],
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 10.0, top: 10),
+            child: Text('Sensors'),
+          ),
+          Expanded(
+            child: GridView.builder(
+              scrollDirection: Axis.vertical,
+              itemCount: 4,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemBuilder: (context, index) {
+                return PlantSensorComp();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/util/sensor_plant.dart
+++ lib/util/sensor_plant.dart
@@ -5,6 +5,29 @@ class PlantSensorComp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 70,
+        decoration: BoxDecoration(
+          color: Colors.blueGrey[200],
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text('PH'),
+                Icon(Icons.grid_3x3),
+              ],
+            ),
+            Text('Value'),
+            Text('Status'),
+          ],
+        ),
+      ),
+    );
   }
 }
